e506445cc166c869e0673dac608491cb5b9b1af8
gaoyanan0331 <1105142987@qq.com>
2017-06-27 14:30:08 +0800

    Initial commit

---
+++ app/src/main/java/kf/gyn/com/supermarket_6/MainActivity.java
@@ -0,0 +1,13 @@
+package kf.gyn.com.supermarket_6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
